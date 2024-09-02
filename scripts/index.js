b8066c42d414623ff708bba3b8934a9b286e0112
sergio11iw <sergio11iw@mail.ru>
2024-09-02 22:18:02 +0500

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+document.getElementById("myRange").addEventListener("change", function() {
+    document.getElementById('fon').style.backgroundColor = `rgb(${255-this.value}, ${255-this.value}, ${255-this.value})`;
+    document.getElementById('block1').style.left = `${80/255*this.value}%`;
+    document.getElementById('block2').style.left = `${100-this.value}%`;
+});
+
+
+
+
+
