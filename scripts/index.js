e101703095ae4fc5d0a2fce48e987769e6739126
sergio11iw <sergio11iw@mail.ru>
2024-09-02 23:13:14 +0500

    Изменения

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 document.getElementById("myRange").addEventListener("change", function() {
     document.getElementById('fon').style.backgroundColor = `rgb(${255-this.value}, ${255-this.value}, ${255-this.value})`;
     document.getElementById('block1').style.left = `${80/255*this.value}%`;
-    document.getElementById('block2').style.left = `${100-this.value}%`;
+    document.getElementById('block2').style.left = `${100-(100/255*this.value)}%`;
 });
 
 
