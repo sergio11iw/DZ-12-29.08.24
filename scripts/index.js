2c63b830992e3cc13c39c399ee45dc6604ab6e14
sergio11iw <sergio11iw@mail.ru>
2024-09-04 00:15:48 +0500

    v.2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,5 @@
 document.getElementById("myRange").addEventListener("change", function() {
     document.getElementById('fon').style.backgroundColor = `rgb(${255-this.value}, ${255-this.value}, ${255-this.value})`;
-    document.getElementById('block1').style.left = `${70/255*this.value}%`;
-    document.getElementById('block2').style.left = `${70-(100/255*this.value)}%`;
+    document.getElementById('block1').style.right = `${(75/255*this.value)}%`;
+    document.getElementById('block2').style.left = `${(75/255*this.value)}%`;
 });
-
-
-
-
-
